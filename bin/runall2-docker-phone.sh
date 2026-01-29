0add3144ff19745c063eb2cdd7ebb4f1cca26e97
Enes Yıldırım <info.enesyildirim@gmail.com>
2026-01-29 12:01:59 +0300

    Revert "chore: update runall2 shell commands"
    This reverts commit 17222fa6ec1fcb0e6668f44377bfd73ce1063ca8.

--- bin/runall2-docker-phone.sh
+++ bin/runall2-docker-phone.sh
@@ -1,57 +1,57 @@
 #!/usr/bin/env bash
 
 echo "run idgen ..."
-nohup ./idgen -f=../etc/idgen.yaml >> ../logs/idgen.log  2>&1 &
+nohup ./idgen -f=../etc2/idgen.yaml >> ../logs/idgen.log  2>&1 &
 sleep 1
 
 echo "run status ..."
-nohup ./status -f=../etc/status.yaml >> ../logs/status.log  2>&1 &
+nohup ./status -f=../etc2/status.yaml >> ../logs/status.log  2>&1 &
 sleep 1
 
 echo "run authsession ..."
-nohup ./authsession -f=../etc/authsession.yaml >> ../logs/authsession.log  2>&1 &
+nohup ./authsession -f=../etc2/authsession.yaml >> ../logs/authsession.log  2>&1 &
 sleep 1
 
 echo "run dfs ..."
-nohup ./dfs -f=../etc/dfs.yaml >> ../logs/dfs.log  2>&1 &
+nohup ./dfs -f=../etc2/dfs.yaml >> ../logs/dfs.log  2>&1 &
 sleep 1
 
 echo "run media ..."
-nohup ./media -f=../etc/media.yaml >> ../logs/media.log  2>&1 &
+nohup ./media -f=../etc2/media.yaml >> ../logs/media.log  2>&1 &
 sleep 1
 
 echo "run biz ..."
-nohup ./biz -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/biz.yaml >> ../logs/biz.log  2>&1 &
+nohup ./biz -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/biz.yaml >> ../logs/biz.log  2>&1 &
 sleep 1
 
 echo "run msg ..."
-nohup ./msg -f=../etc/msg2.yaml >> ../logs/msg.log  2>&1 &
+nohup ./msg -f=../etc2/msg2.yaml >> ../logs/msg.log  2>&1 &
 sleep 1
 
 echo "run sync ..."
-nohup ./sync -f=../etc/sync.yaml >> ../logs/sync.log  2>&1 &
+nohup ./sync -f=../etc2/sync.yaml >> ../logs/sync.log  2>&1 &
 sleep 1
 
 echo "run sfu ..."
-nohup ./sfu -f=../etc/sfu.yaml >> ../logs/sfu.log  2>&1 &
+nohup ./sfu -f=../etc2/sfu.yaml >> ../logs/sfu.log  2>&1 &
 sleep 1
 
 echo "run phone ..."
-nohup ./phone -f=../etc/phone.yaml >> ../logs/phone.log  2>&1 &
+nohup ./phone -f=../etc2/phone.yaml >> ../logs/phone.log  2>&1 &
 sleep 1
 
 echo "run bff ..."
-nohup ./bff -predefined=false -predefined2=false -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/bff.yaml >> ../logs/bff.log  2>&1 &
+nohup ./bff -predefined=false -predefined2=false -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/bff.yaml >> ../logs/bff.log  2>&1 &
 sleep 5
 
 echo "run session ..."
-nohup ./session -f=../etc/session-phone.yaml >> ../logs/session.log  2>&1 &
+nohup ./session -f=../etc2/session-phone.yaml >> ../logs/session.log  2>&1 &
 sleep 1
 
 echo "run gateway ..."
 #nohup ./gateway -f=../etc/gateway.yaml >> ../logs/gateway.log  2>&1 &
-GNET_LOGGING_LEVEL=-1 nohup ./gnetway -f=../etc/gnetway.yaml >> ../logs/gnetway.log 2>&1 &
+GNET_LOGGING_LEVEL=-1 nohup ./gnetway -f=../etc2/gnetway.yaml >> ../logs/gnetway.log 2>&1 &
 sleep 1
 
 echo "run turnserver ..."
-turnserver -v -r 213.250.137.204:3478 -a -o -c ../etc/turnserver.conf --log-file ../logs/turnserver.log
+turnserver -v -r 213.250.137.204:3478 -a -o -c ../etc2/turnserver.conf --log-file ../logs/turnserver.log
