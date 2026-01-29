17222fa6ec1fcb0e6668f44377bfd73ce1063ca8
Enes Yıldırım <info.enesyildirim@gmail.com>
2026-01-29 11:06:01 +0300

    chore: update runall2 shell commands

--- bin/runall2-docker-enterprise-phone.sh
+++ bin/runall2-docker-enterprise-phone.sh
@@ -1,147 +1,147 @@
 #!/usr/bin/env bash
 
 echo "run idgen ..."
-nohup ./idgen -f=../etc2/idgen.yaml >> ../logs/idgen.log  2>&1 &
+nohup ./idgen -f=../etc/idgen.yaml >> ../logs/idgen.log  2>&1 &
 sleep 1
 
 echo "run status ..."
-nohup ./status -f=../etc2/status.yaml >> ../logs/status.log  2>&1 &
+nohup ./status -f=../etc/status.yaml >> ../logs/status.log  2>&1 &
 sleep 1
 
 echo "run authsession ..."
-nohup ./authsession -f=../etc2/authsession.yaml >> ../logs/authsession.log  2>&1 &
+nohup ./authsession -f=../etc/authsession.yaml >> ../logs/authsession.log  2>&1 &
 sleep 1
 
 echo "run dfs ..."
-nohup ./dfs -f=../etc2/dfs.yaml >> ../logs/dfs.log  2>&1 &
+nohup ./dfs -f=../etc/dfs.yaml >> ../logs/dfs.log  2>&1 &
 sleep 1
 
 echo "run media ..."
-nohup ./media -f=../etc2/media.yaml >> ../logs/media.log  2>&1 &
+nohup ./media -f=../etc/media.yaml >> ../logs/media.log  2>&1 &
 sleep 1
 
 echo "run poll ..."
-nohup ./poll -f=../etc2/poll.yaml >> ../logs/poll.log  2>&1 &
+nohup ./poll -f=../etc/poll.yaml >> ../logs/poll.log  2>&1 &
 sleep 1
 
 echo "run twofa ..."
-nohup ./twofa -f=../etc2/twofa.yaml >> ../logs/twofa.log  2>&1 &
+nohup ./twofa -f=../etc/twofa.yaml >> ../logs/twofa.log  2>&1 &
 sleep 1
 
 echo "run biz ..."
-nohup ./biz -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/biz.yaml >> ../logs/biz.log  2>&1 &
+nohup ./biz -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/biz.yaml >> ../logs/biz.log  2>&1 &
 sleep 1
 
 echo "run notify ..."
-nohup ./notify -f=../etc2/notify.yaml >> ../logs/notify.log  2>&1 &
+nohup ./notify -f=../etc/notify.yaml >> ../logs/notify.log  2>&1 &
 sleep 1
 
 echo "run webpage ..."
-nohup ./webpage -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/webpage.yaml >> ../logs/webpage.log  2>&1 &
+nohup ./webpage -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/webpage.yaml >> ../logs/webpage.log  2>&1 &
 sleep 1
 
 #echo "run adminlog ..."
-#nohup ./adminlog -f=../etc2/adminlog.yaml >> ../logs/adminlog.log  2>&1 &
+#nohup ./adminlog -f=../etc/adminlog.yaml >> ../logs/adminlog.log  2>&1 &
 #sleep 1
 
 echo "run inbox ..."
-nohup ./inbox -f=../etc2/inbox.yaml >> ../logs/inbox.log 2>&1 &
+nohup ./inbox -f=../etc/inbox.yaml >> ../logs/inbox.log 2>&1 &
 sleep 1
 
 echo "run msg ..."
-nohup ./msg -f=../etc2/msg.yaml >> ../logs/msg.log  2>&1 &
+nohup ./msg -f=../etc/msg.yaml >> ../logs/msg.log  2>&1 &
 sleep 1
 
 #echo "run channelinbox ..."
-#nohup ./channelinbox -f=../etc2/channelinbox.yaml >> ../logs/channelinbox.log  2>&1 &
+#nohup ./channelinbox -f=../etc/channelinbox.yaml >> ../logs/channelinbox.log  2>&1 &
 #sleep 1
 
 #echo "run channelmsg ..."
-#nohup ./channelmsg -f=../etc2/channelmsg.yaml >> ../logs/channelmsg.log  2>&1 &
-#nohup ./channelmsg -f=../etc2/channelmsg2.yaml >> ../logs/channelmsg.log  2>&1 &
+#nohup ./channelmsg -f=../etc/channelmsg.yaml >> ../logs/channelmsg.log  2>&1 &
+#nohup ./channelmsg -f=../etc/channelmsg2.yaml >> ../logs/channelmsg.log  2>&1 &
 #sleep 1
 
 #echo "run channelmsg2 ..."
-#nohup ./channelmsg2 -f=../etc2/channelmsg2.yaml >> ../logs/channelmsg2.log  2>&1 &
+#nohup ./channelmsg2 -f=../etc/channelmsg2.yaml >> ../logs/channelmsg2.log  2>&1 &
 #sleep 1
 
 echo "run scheduledmessage ..."
-nohup ./scheduledmessage -f=../etc2/scheduledmessage.yaml >> ../logs/scheduledmessage.log  2>&1 &
+nohup ./scheduledmessage -f=../etc/scheduledmessage.yaml >> ../logs/scheduledmessage.log  2>&1 &
 sleep 1
 
 echo "run sync ..."
-nohup ./sync -f=../etc2/sync.yaml >> ../logs/sync.log  2>&1 &
+nohup ./sync -f=../etc/sync.yaml >> ../logs/sync.log  2>&1 &
 sleep 1
 
 #echo "run channelsync ..."
-#nohup ./channelsync -f=../etc2/channelsync.yaml >> ../logs/channelsync.log  2>&1 &
+#nohup ./channelsync -f=../etc/channelsync.yaml >> ../logs/channelsync.log  2>&1 &
 #sleep 1
 
 #echo "run botsync ..."
-#nohup ./botsync -f=../etc2/botsync.yaml >> ../logs/botsync.log  2>&1 &
+#nohup ./botsync -f=../etc/botsync.yaml >> ../logs/botsync.log  2>&1 &
 #sleep 1
 
 echo "run scheduled ..."
-nohup ./scheduled -f=../etc2/scheduled.yaml >> ../logs/scheduled.log  2>&1 &
+nohup ./scheduled -f=../etc/scheduled.yaml >> ../logs/scheduled.log  2>&1 &
 sleep 1
 
 echo "run push ..."
-nohup ./push -f=../etc2/push.yaml >> ../logs/push.log  2>&1 &
+nohup ./push -f=../etc/push.yaml >> ../logs/push.log  2>&1 &
 sleep 1
 
 echo "run sfu ..."
-nohup ./sfu -f=../etc2/sfu.yaml >> ../logs/sfu.log  2>&1 &
+nohup ./sfu -f=../etc/sfu.yaml >> ../logs/sfu.log  2>&1 &
 sleep 1
 
 #echo "run botfather ..."
-#nohup ./botfather -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/botfather.yaml >> ../logs/botfather.log  2>&1 &
+#nohup ./botfather -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/botfather.yaml >> ../logs/botfather.log  2>&1 &
 #sleep 1
 
 #echo "run gif ..."
-#nohup ./gif -f=../etc2/gif.yaml >> ../logs/gif.log  2>&1 &
+#nohup ./gif -f=../etc/gif.yaml >> ../logs/gif.log  2>&1 &
 #sleep 1
 
 echo "run historyttl ..."
-nohup ./historyttl -f=../etc2/historyttl.yaml >> ../logs/historyttl.log  2>&1 &
+nohup ./historyttl -f=../etc/historyttl.yaml >> ../logs/historyttl.log  2>&1 &
 sleep 1
 
 #echo "run bots ..."
-#nohup ./bots -f=../etc2/bots.yaml >> ../logs/bots.log  2>&1 &
+#nohup ./bots -f=../etc/bots.yaml >> ../logs/bots.log  2>&1 &
 #sleep 1
 
 echo "run bff ..."
-nohup ./bff -predefined=false -predefined2=false -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc2/bff.yaml >> ../logs/bff.log  2>&1 &
+nohup ./bff -predefined=false -predefined2=false -app_name="Teamgram" -t.me="teamgram.me" -site_name="teamgram.net" -f=../etc/bff.yaml >> ../logs/bff.log  2>&1 &
 echo "Waiting for bff to register with etcd..."
 sleep 15
 
 echo "run phone ..."
-nohup ./phone -f=../etc2/phone.yaml >> ../logs/phone.log  2>&1 &
+nohup ./phone -f=../etc/phone.yaml >> ../logs/phone.log  2>&1 &
 echo "Waiting for phone to register with etcd..."
 sleep 15
 
 echo "run session ..."
-nohup ./session -f=../etc2/session-enterprise-phone.yaml >> ../logs/session.log  2>&1 &
+nohup ./session -f=../etc/session-enterprise-phone.yaml >> ../logs/session.log  2>&1 &
 sleep 5
 
 echo "run gateway ..."
-#nohup ./gateway -f=../etc2/gateway.yaml >> ../logs/gateway.log  2>&1 &
-GNET_LOGGING_LEVEL=-1 nohup ./gnetway -f=../etc2/gnetway.yaml >> ../logs/gnetway.log 2>&1 &
+#nohup ./gateway -f=../etc/gateway.yaml >> ../logs/gateway.log  2>&1 &
+GNET_LOGGING_LEVEL=-1 nohup ./gnetway -f=../etc/gnetway.yaml >> ../logs/gnetway.log 2>&1 &
 sleep 1
 
 #echo "run tgadmin ..."
-#nohup ./tgadmin -f=../etc2/tgadmin.yaml >> ../logs/tgadmin.log  2>&1 &
+#nohup ./tgadmin -f=../etc/tgadmin.yaml >> ../logs/tgadmin.log  2>&1 &
 #sleep 1
 
 #echo "run tgme ..."
-#nohup ./tgme -f=../etc2/tgme.yaml >> ../logs/tgme.log  2>&1 &
+#nohup ./tgme -f=../etc/tgme.yaml >> ../logs/tgme.log  2>&1 &
 #sleep 1
 
 #echo "run botway ..."
-#nohup ./botway -f=../etc2/botway.yaml >> ../logs/botway.log  2>&1 &
+#nohup ./botway -f=../etc/botway.yaml >> ../logs/botway.log  2>&1 &
 #sleep 1
 
 echo "run wsserver ..."
-nohup ./wsserver -f=../etc2/wsserver.yaml >> ../logs/wsserver.log 2>&1 &
+nohup ./wsserver -f=../etc/wsserver.yaml >> ../logs/wsserver.log 2>&1 &
 
 echo "run turnserver ..."
-turnserver -v -r 192.168.10.106:3478 -a -o -c ../etc2/turnserver.conf --log-file ../logs/turnserver.log
+turnserver -v -r 192.168.10.106:3478 -a -o -c ../etc/turnserver.conf --log-file ../logs/turnserver.log
