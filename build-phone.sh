6b8e35f2610f257b098fc112c5f46c548b365d3d
Enes Yıldırım <info.enesyildirim@gmail.com>
2026-01-30 15:52:24 +0300

    chore: add admin build to build-phone.sh

--- build-phone.sh
+++ build-phone.sh
@@ -5,7 +5,7 @@ GITHUB="/opt/data/teamgram/src/github.com"
 TEAMGRAMIO="/opt/data/teamgram/src/teamgram.io"
 
 cd ${GITHUB}/teamgram/teamgram-server
-make -f Makefile.teamgram idgen status dfs media authsession msg sync session gnetway
+make -f Makefile.teamgram idgen status dfs media authsession msg sync session gnetway admin
 
 cd ${TEAMGRAMIO}/teamcalls-server
 make sfu phone bff biz
